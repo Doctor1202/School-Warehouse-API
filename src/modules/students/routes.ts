f49d8da53e2ae5ca08b0129c58d4ffa79424467c
Doc <zdrozda1202@gmail.com>
2026-06-01 14:50:11 +0300

    Refactoring routes/student

--- src/modules/students/routes.ts
+++ src/modules/students/routes.ts
@@ -7,17 +7,19 @@ import { studentParamsSchema, studentSearchSchema, studentsSchema } from "./sche
 export const studentsRoute = new Elysia();
 
 studentsRoute
-  .get("/students", async ({ query }) => {
-    const parseQuaery = studentSearchSchema.parse(query);
+  .get("/students", async ({ query, status}) => {
+    const parseQuaery = studentSearchSchema.safeParse(query);
 
-    if (parseQuaery.search || parseQuaery.classId) {
+    if(!parseQuaery.success) return status (400, "Invalid payload");
+
+    if (parseQuaery.data.search || parseQuaery.data.classId) {
       const filteredStudents = await db
         .select()
         .from(students)
         .where(
           and(
-            parseQuaery.search ? ilike(students.fullName, `%${parseQuaery.search}%`) : undefined,
-            parseQuaery.classId ? eq(students.classId, parseQuaery.classId) : undefined,
+            parseQuaery.data.search ? ilike(students.fullName, `%${parseQuaery.data.search}%`) : undefined,
+            parseQuaery.data.classId ? eq(students.classId, parseQuaery.data.classId) : undefined,
           ),
         );
 
@@ -28,82 +30,64 @@ studentsRoute
 
     return allStudents;
   })
-  .get("/students/:id", async ({ params, set }) => {
-    try {
-      const { id } = studentParamsSchema.parse(params);
+  .get("/students/:id", async ({ params, status }) => {
+      const id = studentParamsSchema.safeParse(params);
 
-      const result = await db.select().from(students).where(eq(students.id, id)).limit(1);
+      if (!id.success) {
+        return status(400, "Invalid params")
+      }
 
-      if (!result[0]) {
-        set.status = 404;
-        return { error: "Student not found" };
+      const [result] = await db.select().from(students).where(eq(students.id, id.data.id)).limit(1);
+
+      if (!result) {
+        return status(404, "Student not found");
       }
 
       return result;
-    } catch {
-      set.status = 400;
-      return { error: "Invalid params" };
-    }
   })
-  .post("/students", async ({ body, set }) => {
-    try {
-      const data = studentsSchema.parse(body);
+  .post("/students", async ({ body, status }) => {
+      const data = studentsSchema.safeParse(body);
 
-      const existingClass = await db.select().from(classes).where(eq(classes.id, data.classId)).limit(1);
+      if(!data.success) return status(400, "Invalid payload");
 
-      const existingClassId = existingClass[0];
+      const [existingClass] = await db.select().from(classes).where(eq(classes.id, data.data.classId)).limit(1);
 
-      if (!existingClassId) {
-        set.status = 404;
-        return { error: "Class not found" };
-      }
-
-      if (existingClass.length === 0) {
-        set.status = 400;
-        return { error: "Class not found" };
+      if (!existingClass) {
+        return status(404, "Class not found");
       }
 
       const [newStudent] = await db
         .insert(students)
         .values({
-          fullName: data.fullName,
-          classId: data.classId,
+          fullName: data.data.fullName,
+          classId: data.data.classId,
         })
         .returning();
 
       return newStudent;
-    } catch {
-      set.status = 400;
-      return { error: "Invalid payload" };
-    }
   })
-  .patch("/students/:id", async ({ body, params, set }) => {
-    try {
-      const { id } = studentParamsSchema.parse(params);
-      const data = studentsSchema.parse(body);
+  .patch("/students/:id", async ({ body, params, status }) => {
+      const id = studentParamsSchema.safeParse(params);
+      if(!id.success)return status (400, "Invalid payload");
+
+      const data = studentsSchema.safeParse(body);
+      if(!data.success)return status (400, "Invalid payload");
+
       const studentUpdate = await db
         .update(students)
-        .set({ fullName: data.fullName, classId: data.classId })
-        .where(eq(students.id, id))
+        .set({ fullName: data.data.fullName, classId: data.data.classId })
+        .where(eq(students.id, id.data.id))
         .returning();
 
       return studentUpdate;
-    } catch {
-      set.status = 400;
-      return { error: "Invalid payload" };
-    }
   })
-  .delete("/students/:id", async ({ params, set }) => {
-    try {
-      const { id } = studentParamsSchema.parse(params);
-      const studentDeleteResult = await db.delete(students).where(eq(students.id, id)).returning();
-      if (!studentDeleteResult[0]) {
-        set.status = 404;
-        return { error: "Student not found" };
-      }
+  .delete("/students/:id", async ({ params, status }) => {
+      const id = studentParamsSchema.safeParse(params);
+      if(!id.success) return status (400, "Invalid payload");
+
+      const studentDeleteResult = await db.delete(students).where(eq(students.id, id.data.id)).returning();
+      if (!studentDeleteResult[0]) return status(404, "Student not found");
+      
       return studentDeleteResult;
-    } catch {
-      set.status = 400;
-      return { error: "Invalid payload" };
-    }
+
   });
